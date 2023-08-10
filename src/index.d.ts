f8a08d7aa68985aa86099e2a2b660d65fdcff23e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-10 20:55:32 +0600

    fix: add types (#71)
    * add types
    
    * Update deps
    
    Update package.json
    
    Update package.json
    
    Update package-lock.json

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+import type { marked } from 'marked';
+import type { MarkedExtension } from 'marked';
+
+/**
+ * Add closing slash to tags like: `hr`, `br`, `img`, and `input`
+ *
+ * @returns A {@link MarkedExtension | MarkedExtension} to be passed to {@link marked.use | `marked.use()`}
+ */
+export function markedSmartypants(): MarkedExtension;
