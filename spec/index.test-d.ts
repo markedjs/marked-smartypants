ab792dfb0e280d393eca03692338a642000dca64
Nathan Arthur <nathan@nathanarthur.com>
2023-08-18 13:29:32 -0400

    update types

--- spec/index.test-d.ts
+++ spec/index.test-d.ts
@@ -1,6 +1,9 @@
-import { marked } from 'marked';
-import { markedSmartypants } from '../src/index';
+import { marked } from "marked";
+import { markedSmartypants } from "../src/index";
 
 marked.use(markedSmartypants());
+marked.use(markedSmartypants({}));
+marked.use(markedSmartypants({ config: "1" }));
+marked.use(markedSmartypants({ config: 1 }));
 
-marked.parse('He said, -- "A \'simple\' sentence. . ." --- unknown');
+marked.parse("He said, -- \"A 'simple' sentence. . .\" --- unknown");
